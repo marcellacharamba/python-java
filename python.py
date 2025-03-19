f7809670e3d2c4b152d74db334cd766f8f3df010
marcellacharamba <161905045+marcellacharamba@users.noreply.github.com>
2025-03-18 21:43:20 -0300

    Add files via upload

---
+++ python.py
@@ -0,0 +1,11 @@
+print("Que tal criarmos uma história! Responda às perguntas a seguir:")
+
+lugar = input("Onde a história acontece? ")
+personagem = input("Quem é o personagem principal? ")
+objeto = input("O que o personagem encontrou? ")
+lugar_objeto = input("Onde estava esse objeto? ")
+
+historia = f"Em um belo dia, {personagem} estava em {lugar} quando, de repente, encontrou {objeto} {lugar_objeto}, e de repente a bruxa do 71 apareceu, montada em uma vassoura e dominou o mundo."
+
+print("\nAqui está a sua história:")
+print(historia)
