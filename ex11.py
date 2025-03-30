83d9db70302cc99bae2e9bf926fd151f92c58ae7
marcellacharamba <161905045+marcellacharamba@users.noreply.github.com>
2025-03-29 22:22:50 -0300

    Delete ex11.py

--- ex11.py
+++
@@ -1,7 +0,0 @@
-usuario = input("Digite o nome de usuário: ")
-senha = input("Digite a senha: ")
-
-if usuario == senha:
-    print("Erro: O nome de usuário e a senha não podem ser iguais.")
-else:
-    print("Login realizado com sucesso!")
