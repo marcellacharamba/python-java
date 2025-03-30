8a5cf5e7704ac57890a5e5a3f52677246e181608
marcellacharamba <161905045+marcellacharamba@users.noreply.github.com>
2025-03-29 22:22:41 -0300

    Delete ex12.py

--- ex12.py
+++
@@ -1,9 +0,0 @@
-quantidade_turmas = int(input("Digite a quantidade de turmas: "))
-total_alunos = int(input("Digite a quantidade total de alunos: "))
-
-media_alunos = total_alunos / quantidade_turmas
-
-print(f"A média de alunos por turma é: {media_alunos:.2f}")
-
-if media_alunos > 40:
-    print("Aviso: Alguma turma tem mais de 40 alunos.")
